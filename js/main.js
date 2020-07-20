79585854f50b58abc0b7d2d0edda42a218addeb6
Ramon Audain <raudain123@gmail.com>
2020-07-19 19:09:26 -0700

    Create autocomplete functionality

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const search = document.getElementById("search");
+const matchList = document.getElementById("match-list")
+
+// Search states.json and filter it
+const searchStates = async searchText => {
+    const res = await fetch("../data/states.json");
+    const states = await res.json();
+    
+    // Get matches to current text input
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, "gi")
+        return state.name.match(regex) || state.abbr.match(regex)
+    })
+
+    if(!searchText.length){
+        matches = [];
+        matchList.innerHTML = ""
+    }
+
+    outputHTML(matches)
+    //console.log(matches)
+}
+
+// Show results in HTML
+const outputHTML = matches => {
+    if(matches.length){
+        const html = matches.map(match => `
+        <li><a href="#">${match.name} (${match.abbr})</a></li>`).join("")
+        
+        matchList.innerHTML = html;
+    }
+
+    
+}
+
+search.addEventListener("input", () => searchStates(search.value))
